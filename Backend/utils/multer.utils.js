a431898eda92b0bdbe7d045e83bdbc34124c7091
Harsh-DevOps117 <harsh.240103263@iiitbh.ac.in>
2025-06-22 22:36:48 +0530

    Update multer.utils.js

--- Backend/utils/multer.utils.js
+++ Backend/utils/multer.utils.js
@@ -1,32 +1,18 @@
 import multer from "multer";
-import path from "path";
-
-
-path.resolve("public");
-
-// Temporary storage before uploading to Cloudinary
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public"); // Ensure this folder exists
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
-  },
-});
+import { storage } from "./cloudinary.js";
 
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+  const allowedTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Only jpg, png, and gif images are allowed"), false);
+    cb(new Error("Only JPG, PNG, GIF, and WEBP images are allowed"), false);
   }
 };
 
 const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
+  storage,
+  fileFilter,
   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
 });
 
